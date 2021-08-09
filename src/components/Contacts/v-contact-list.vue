f94bcf31f1a6f97022fe5dd4aa5b842af6848b87
Максим Майнагашев <maxuser99@mail.ru>
2021-08-10 00:16:10 +0700

    View works

---
+++ src/components/Contacts/v-contact-list.vue
@@ -0,0 +1,26 @@
+<template>
+
+    <div class="v-contact-list">
+    <contact-user/>
+    </div>
+</template>
+
+<script>
+import contactUser from './v-contact-user.vue'
+
+export default {
+   name: "v-contact-list",
+   components:{
+       contactUser
+   },
+   props: {},
+   data(){
+       return {}
+   },
+   computed: {}
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/v-main-wrapper.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="v-main-wrapper">
+    <v-header/>
+    <v-bottom-nav/>
+    </div>
+</template>
+
+
+<script>
+import vHeader from './layouts/v-header.vue'
+import vBottomNav from './layouts/v-bottom-nav.vue'
+
+    export default{
+        name:"v-main-wrapper",
+        components:{
+            vHeader,
+            vBottomNav
+        },
+        props:{},
+        data() {
+            return {}
+        },
+        computed: {}
+    }
+</script>
+
+<style scoped>
+
+</style>
